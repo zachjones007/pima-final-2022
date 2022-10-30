2f9e6c64c35373e973571170a7eeff7b8280dc22
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2022-10-30 07:21:55 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,10 @@ test_func1 = print("smoop")
 def myvar():
     print["quack","boob","chungs "]
     print ("hello")
+    
+lines = ['Readme', 'How to write text files in Python']
+with open('readme.txt', 'w') as f:
+    for line in lines:
+        f.write(line)
+        f.write('\n')
 
