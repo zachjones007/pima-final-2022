c6a4e2e62efe521a40beb71d782dc34024c2b696
zach jones <zach@jonesaz.com.com>
2022-10-31 16:41:23 -0700

    fix

--- readme.txt
+++ readme.txt
@@ -1,5 +1,10 @@
-Customer input goes here
-end goal is to make it so customer input put into arrays attached to either name or birthdate.
-use tensor flow and data to guess approxmiate life expexctancy based on inputs
-(data size of 700)
-data should be on one ethinic group should be able to work on another 
+[['e'], 'e', 'e', 'e', 'e']/  
+['e', 'e', 'e', 'e', 'e']/  
+['e', 'e', 'e', 'e', 'e']/  
+['e', 'e', 'e', 'e', 'e', 'e']/  
+['e', 'e', 'e', 'e', 'e', 'e']/  
+[['d'], 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd']/  
+[['d', 'dd'], 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd']/  
+[['d', 'dd', 'd'], 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd']/  
+[['d', 'dd', 'd', 'd'], 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd']/  
+[['d', 'dd', 'd', 'd', 'd'], 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'd']/  
--- use_test.py
+++ use_test.py
@@ -1,45 +1,34 @@
 from datetime import date
 from test import Pet
 from stan import Yes
-
 def megamain():
     animals = []
     animals1 = [animals] 
-    animasl2 = animals1
-
+    animals2 = [animals1 ,animals]
     name = str
-    name1 = input("input pet name")
+      
     while True:
-
         
-
         animal_type = input("input pet type")
         name = input("input pet name")
         age = (input("pet age") + ':')
-
         pet = Pet({'name': name, 'type': animal_type, 'age': age})
-
         animals.append(animal_type)
         print (animals1)
         a = []
         w = 0
         r = 0
-
-        while w<1:
+        while w<3:
                                             
                    
-
             animal_type1 = input ('type' )
-             
+            name1 = input("input pet name") 
             age1 = input("pet age") 
             cost1 = input("cost")
-
             yes = Yes({'name': (name1 + '32'), 'type': animal_type1, 'age': age1 })
-
             print(w)
             print(a)
             w = w+1
-
             animals1.extend(animal_type1)
             animals1.extend(name1)
             animals1.extend(age1)
@@ -48,20 +37,18 @@ def megamain():
         searchname = input('search name, or quit')  
         if searchname == name:
             print(animals)
-            continue1 = input("would you like to Continue? or quit")
-       
-                         
+            continue1 = input("would you like to Continue? or quit") 
+            
+
         w = 0
-
-        animasl2
+        animals2
         with open('readme.txt', 'a') as f:
-                    f.write(str(animals1 ))
-                    f.writelines("/  \n".format(animals1 ))
-        animals1 .clear()
-    for letter in continue1:
-        if letter in "n,N,q,Q":
+                    f.write(str(animals1))
+                    f.writelines("/  \n".format(animals1))
+
+        for letter in continue1:
+                if letter in "n,N,q,Q":
                     quit("quittting")
+                     #import from another file
 
-
-                     
 megamain()
