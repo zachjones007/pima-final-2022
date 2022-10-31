1200f9b6c4634275eaea316aa35a12518e0488d6
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2022-10-31 05:34:46 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-test_func1 = print("smoop")
-def myvar():
-    print["quack","boob","chungs "]
-    print ("hello")
-    
-lines = ['Readme', 'How to write text files in Python']
-with open('readme.txt', 'w') as f:
-    for line in lines:
-        f.write(line)
-        f.write('\n')
 
+class Pet:
+            
+            def __init__(self, options):
+                self._name = options
+
+            def set_name(self, new_name):
+                self._name = new_name
+
+            def get_name(self):
+                return self._name
+
+            def get_animal_type(self):
+                return self._animal_type
+
+            def get_age(self):
+                return self._age
