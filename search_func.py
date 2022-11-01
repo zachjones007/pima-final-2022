d061b7ba9ff1c756e51f3e5d51cb69fa2e9a9062
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2022-11-01 15:36:56 -0700

    Update search_func.py

--- search_func.py
+++ search_func.py
@@ -1,29 +1,31 @@
 # need this only to expect name
 from use_test import megamain
-def search_str(file_path, word):
-    with open(file_path, 'r') as file:
-        # read all content of a file
-        content = file.read()
-        # check if string present in a file
-        if word in content:
-            print('string exist in a file')
-            with open('readme.txt', 'r') as fp:
+def __init__():
+    def search_str(file_path, word):
+        with open(file_path, 'r') as file:
+            # read all content of a file
+            content = file.read()
+            # check if string present in a file
+            if word in content:
+                print('string exist in a file')
+                with open('readme.txt', 'r') as fp:
 
-               lines = fp.readlines()
-               for line in lines:
-                  if line.find(word) != -1:
-                     print(word, 'string exists in file')
-                     print('Line Number:', lines.index(line))
-                     print('Line:', line)
-        else:
-            print('string does not exist in a file')
+                   lines = fp.readlines()
+                   for line in lines:
+                      if line.find(word) != -1:
+                         print(word, 'string exists in file')
+                         print('Line Number:', lines.index(line))
+                         print('Line:', line)
+            else:
+                print('string does not exist in a file')
 
-       
 
-search_str('readme.txt', input('Enter a string to search: '))
-def new_func():
-    return megamain()
 
-new_func()
+    search_str('readme.txt', input('Enter a string to search: '))
+    def new_func():
+        return megamain()
+
+    new_func()
+__init__()
 
 
