7c64b0e70ed10178fd6f33f7ab102be980bebe7e
zach jones <zach@jonesaz.com.com>
2022-11-01 06:27:32 -0700

    added search_func need to import use_test

--- search_func.py
+++ search_func.py
@@ -1,7 +1,22 @@
-def search_func():
-   searchname = input('search name, or quit')   
-   if searchname == name:
-            print(animals)
-   elif searchname == quit:
-            quit("quitting")
-           
+
+def search_str(file_path, word):
+    with open(file_path, 'r') as file:
+        # read all content of a file
+        content = file.read()
+        # check if string present in a file
+        if word in content:
+            print('string exist in a file')
+            with open('readme.txt', 'r') as fp:
+
+               lines = fp.readlines()
+               for line in lines:
+                  if line.find(word) != -1:
+                     print(word, 'string exists in file')
+                     print('Line Number:', lines.index(line))
+                     print('Line:', line)
+        else:
+            print('string does not exist in a file')
+
+       
+
+search_str('readme.txt', input('Enter a string to search: '))
